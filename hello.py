b58ecf82e0872d11f82702a3e1f2afd3bf267e88
Ahmed Gharib <a.gharib89@yahoo.com>
2021-09-12 17:56:33 +0200

    Add search cli function

--- hello.py
+++ hello.py
@@ -1,7 +1,36 @@
-#!/c/Users/aghar/anaconda3/envs/ribo/python
-def add(x, y):
-    return x + y
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import click
+import glob
 
 
-result = add(10, 20)
-print(f"This is ths sum: {10}, {20}, {result}")
+# Command line tool to search for files by type
+@click.command()
+# Option to specify the file path
+@click.option(
+    "--path",
+    default=".",
+    prompt="Enter the file path to search in!",
+    help="Path to search for files",
+)
+# Option to specify the file type
+@click.option(
+    "--ftype",
+    default="*",
+    prompt="Enter the file type to search for!",
+    help="File type to search for",
+)
+def search(path, ftype):
+    """
+    Search for files by type
+    """
+    # Get all files in the path
+    files = glob.glob(path + "/*." + ftype)
+    click.echo(click.style("Found matches:", fg="red"))
+    # Print the files
+    for file in files:
+        click.echo(click.style(file, bg="white", fg="black"))
+
+
+if __name__ == "__main__":
+    search()
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,9 @@
-from hello import add
+from click.testing import CliRunner
+from hello import search
 
 
-def test_add():
-    assert add(1, 2) == 3
+def test_search():
+    runner = CliRunner()
+    result = runner.invoke(search, ["--path", ".", "--ftype", "py"])
+    assert result.exit_code == 0
+    assert ".py" in result.output
